3ba6f846983b931865328fcf740df84985b7ac12
1Filipek2 <rybakovfilip12@gmail.com>
2025-06-03 03:49:18 +0200

    test rozostrenia

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,12 @@
 document.body.style.opacity = 0;
 document.body.style.transition = "opacity 1.2s";
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
-    document.body.style.opacity = 1;
-  }, 100); // malá pauza pre plynulejší efekt
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  const overlay = document.getElementById("black-overlay");
+    document.body.classList.add('fade-in');
+  }, 400); // Krátka pauza pre istotu načítania
   setTimeout(() => {
-    overlay.style.opacity = "0";
-  }, 100); // malá pauza pre istotu
-  setTimeout(() => {
-    overlay.style.display = "none";
-  }, 2100); // skryť po fade-oute
+    const overlay = document.getElementById('black-overlay');
+    if (overlay) overlay.style.display = 'none';
+  }, 2400); // Skry overlay po fade-oute
 });
